69bc7157029873f88b9954fdcdbbf43f2e6b6980
molymumu <mmhamid03@gmail.com>
2019-03-02 17:34:22 -0500

    defination

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -14,6 +14,21 @@ public class MakePyramid {
 
         */
 
-
+        int n = 6;
+        int x = 5 * n;
+        for (int i=0; i<n; i++)
+        {
+            for (int j = 0; j<x; j++)
+            {
+                System.out.print(" ");
+            }
+            x = x-1;
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+    }
   }
+
