9d8f864829d1f8fa4b44a699784c6f98d3e885a5
molymumu <mmhamid03@gmail.com>
2019-03-05 21:40:38 -0500

    design

--- src/design/FortuneEmployee.java
+++ src/design/FortuneEmployee.java
@@ -1,6 +1,12 @@
 package design;
 
+import databases.ConnectToSqlDB;
+
+import java.util.List;
+
 public class FortuneEmployee {
+
+	private static List<String> storePattern;
 
 	/**
 	 * FortuneEmployee class has a main methods where you will be able to create Object from
@@ -13,9 +19,23 @@ public class FortuneEmployee {
 	 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 	 *
 	 **/
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		EmployeeInfo Einfo1 = new EmployeeInfo();
+		EmployeeInfo Einfo2 = new EmployeeInfo(777);
+		Einfo2.Parson();
+		Einfo2.assignDepartment();
+		Einfo2.calculateSalary();
+		Einfo2.calculateEmployeeBonus(2);
+		Einfo2.calculateEmployeePension();
+		EmployeeInfo Einfo3 = new EmployeeInfo("Mohammad Hamid",107);
 
+
+
+		ConnectToSqlDB connectDB = new ConnectToSqlDB();
+		int[] array1 = new int[0];
+		ConnectToSqlDB.insertDataFromArrayToSqlTable(array1, "tb2_Parson", "column_FortuneEmployee");
+		storePattern = ConnectToSqlDB.readDataBase("tb2_Parson", "column_FortuneEmployee");
+		System.out.println("Data is being read from the Table (tableParson) and displaying to the console");
 
 	}
 
---
+++ src/design/Parson.java
@@ -0,0 +1,14 @@
+package design;
+
+public abstract class Parson {
+    final String dept1 = "QTP";
+    final String dept2 ="Selenium";
+
+    final String dept3 = "Qa";
+    final String dept4="Finance";
+    public abstract void Parson();
+
+    public abstract void AllDepart();
+}
+
+
